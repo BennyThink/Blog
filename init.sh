ada390abfa770fe5757e76bf79343e5cba12aec6
BennyThink <benny.think@gmail.com>
2020-12-21 19:17:12 +0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+mkdir -p ./certs/dmesg.app
+cat /etc/ssl/certs/dhparam.pem > ./certs/dhparam.pem
+cat /etc/letsencrypt/live/dmesg.app/fullchain.pem > ./certs/dmesg.app/fullchain.pem
+cat /etc/letsencrypt/live/dmesg.app/privkey.pem > ./certs/dmesg.app/privkey.pem
