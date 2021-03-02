4f047cabb0c957d8a2436adc4ddd1c38c9c27ff6
BennyThink <benny.think@gmail.com>
2021-03-02 23:05:27 +0800

    add cf certs commands

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 mkdir www
 mkdir  ./certs
 cat /etc/ssl/certs/dhparam.pem > ./certs/dhparam.pem
-cat /etc/ssl/cf/dmesg_cf_cert.pem > ./certs/dmesg.app/fullchain.pem
-cat /etc/ssl/cf/dmesg_cf_key.pem > ./certs/dmesg.app/privkey.pem
+cat /etc/ssl/cf/dmesg_cf_cert.pem > ./certs/fullchain.pem
+cat /etc/ssl/cf/dmesg_cf_key.pem > ./certs/privkey.pem
