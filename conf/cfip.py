0cbfedcb704df0a5bcb1e5e39023cd0478213b36
BennyThink <benny.think@gmail.com>
2021-04-09 21:08:17 +0800

    update cf ip
    Signed-off-by: BennyThink <benny.think@gmail.com>

--- conf/cfip.py
+++ conf/cfip.py
@@ -12,13 +12,16 @@ import requests
 v4 = requests.get("https://www.cloudflare.com/ips-v4").text
 v6 = requests.get("https://www.cloudflare.com/ips-v6").text
 config = ""
-
+v4_geo = ""
+v6_geo = ""
 for item in v4.split():
     directive = f"set_real_ip_from {item};\n"
+    v4_geo += f"{item}  1;\n"
     config += directive
 config += "\n"
 for item in v6.split():
     directive = f"set_real_ip_from {item};\n"
+    v6_geo += f"{item}  1;\n"
     config += directive
 
 bottom = "real_ip_header CF-Connecting-IP;"
@@ -26,3 +29,14 @@ config += "\n" + bottom
 
 with open("cloudflare.conf", "w")as f:
     f.write(config)
+
+only_cf = f"""
+geo $realip_remote_addr $cloudflare_ip {{
+    default          0;
+    {v4_geo}
+    {v6_geo}
+}}
+"""
+
+with open("block_non_cf.conf","w") as f:
+    f.write(only_cf)
